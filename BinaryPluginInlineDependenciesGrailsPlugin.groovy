d33fa21bc2564052e64dd9d4deeec3148daa6151
Alex Anderson <aganderson@cantab.net>
2013-04-11 20:22:16 +0300

    Added Burt Beckwith to plugin developers list

--- BinaryPluginInlineDependenciesGrailsPlugin.groovy
+++ BinaryPluginInlineDependenciesGrailsPlugin.groovy
@@ -3,6 +3,7 @@ class BinaryPluginInlineDependenciesGrailsPlugin {
 	def grailsVersion = "2.0 > *"
 	def title = 'Plugin Inline Dependencies'
 	def author = 'Alex Anderson'
+	def developers = [[name:'Burt Beckwith']]
 	def authorEmail = ''
 	def description = 'Squash all the dependencies in your lib directory into your plugin at package time.'
 	def documentation = 'http://github.com/frontlinesms/grails-binary-plugin-inline-dependencies-plugin'
